5392a53dd1fbee553002c5bcebba01dae4681f4a
Sean Riedel <riedelsean@gmail.com>
2026-09-11 22:36:50 -0700

    starting GP prediction vector function

--- src/GP_volAvgToPointPredVect.f90
+++ src/GP_volAvgToPointPredVect.f90
@@ -1,15 +1,46 @@
 module GP_volAvgToPointPredVect
     
-    use definitions, only: ndim, qp, qp_pi
+    use definitions, only: qp, ndim
+    use GP_volAvgToVolAvgSqExpKernel
+    use GP_volAvgToPointSqExpKernel
 
     implicit none
 
 contains
 
-    function GP_volAvgToPointPredVect_(x1, x2) result(predVect)
-        
+    function GP_volAvgToPointPredVect_(XX, xstr, nStenc, nPred, dl, ell) result(predVect)
+        ! function:     GP_volAvgToPointPredVect_
+        ! purpose:      
+        !               
+        !               
+        ! 
+        ! Inputs:       
+        !               
+        ! Outputs:      
+        ! ------------------------------------------------------------
 
+        implicit none
+        integer, intent(in) :: nStenc, nPred
+        real(qp), intent(in), dimension(ndim, nStenc) :: XX
+        real(qp), intent(in), dimension(ndim, nPred) :: xstr
+        real(qp), intent(in) :: dl(ndim), ell(ndim)
+        real(qp) :: predVect(nPred, nStenc)
 
+        ! local variables
+        integer :: i, j
+        real(qp) :: Kcov(nStenc, nStenc), Kstr(nPred, nStenc)
+
+        do i=1,nStenc
+            do j=1,nStenc
+                Kcov(i,j) = GP_volAvgToVolAvgSqExpKernel_(XX(:,i), XX(:,j), dl, ell)
+            end do
+        end do
+
+        do i=1,nPred
+            do j=1,nStenc
+                Kstr(i,j) = GP_volAvgToPointSqExpKernel_(XX(:,i), XX(:,j), dl, ell)
+            end do
+        end do
 
     end function GP_volAvgToPointPredVect_
 
