fac983fdba3eff3d6c8702959b786bf754856cd0
Sean Riedel <riedelsean@gmail.com>
2026-09-14 17:19:54 -0700

    Untested (but as far as I know correct) subroutine to compute the GP prediction vectors for predicting pointwise values from voluem averaged quantities

--- src/GP_volAvgToPointPredVect.f90
+++ src/GP_volAvgToPointPredVect.f90
@@ -3,32 +3,41 @@ module GP_volAvgToPointPredVect
     use definitions, only: qp, ndim
     use GP_volAvgToVolAvgSqExpKernel
     use GP_volAvgToPointSqExpKernel
+    use linAlgQuadPrecision, only: linAlgQuadPrecision_solveSPD
 
     implicit none
 
 contains
 
-    function GP_volAvgToPointPredVect_(XX, xstr, nStenc, nPred, dl, ell) result(predVect)
+    function GP_volAvgToPointPredVect_(XX, XXstr, nStenc, nPred, dl, ell) result(predVect)
         ! function:     GP_volAvgToPointPredVect_
-        ! purpose:      
+        ! purpose:      Construct the Gaussian process prediction vector(s) for predicting 
+        !               pointwise data given volume averaged data.
         !               
+        ! Inputs:       - XX(ndim, nStenc) (quadruple precision real) the cell
+        !                 center coordinates of all of the cells in the GP stencil
+        !               - XXstr(ndim, nPred) (quadruple precision real) the coordinates of the points
+        !                 at which we want to predict pointwise data
+        !               - nStenc (integer) the number of cells in the GP stencil
+        !               - nPred (integer) the number of points at which we want to predict pointwise data
+        !               - dl(ndim) (quadruple precision real) the grid spacing. dl(1) = dx, dl(2) = dy
+        !               - ell is the hyperparameter of the squared exponential kernel
         !               
-        ! 
-        ! Inputs:       
-        !               
-        ! Outputs:      
+        ! Outputs:      - predVect(nStenc, nPred) the GP prediction vector(s).
+        !                 The ith column of predVect is the prediction vector for
+        !                 the ith point at which we want to predict pointwise data.
         ! ------------------------------------------------------------
 
         implicit none
         integer, intent(in) :: nStenc, nPred
         real(qp), intent(in), dimension(ndim, nStenc) :: XX
-        real(qp), intent(in), dimension(ndim, nPred) :: xstr
+        real(qp), intent(in), dimension(ndim, nPred) :: XXstr
         real(qp), intent(in) :: dl(ndim), ell(ndim)
-        real(qp) :: predVect(nPred, nStenc)
+        real(qp) :: predVect(nStenc, nPred)
 
         ! local variables
         integer :: i, j
-        real(qp) :: Kcov(nStenc, nStenc), Kstr(nPred, nStenc)
+        real(qp) :: Kcov(nStenc, nStenc), Kstr(nStenc, nPred)
 
         do i=1,nStenc
             do j=1,nStenc
@@ -36,11 +45,13 @@ contains
             end do
         end do
 
-        do i=1,nPred
-            do j=1,nStenc
-                Kstr(i,j) = GP_volAvgToPointSqExpKernel_(XX(:,i), XX(:,j), dl, ell)
+        do i=1,nStenc
+            do j=1,nPred
+                Kstr(i,j) = GP_volAvgToPointSqExpKernel_(XX(:,i), XXstr(:,j), dl, ell)
             end do
         end do
+
+        predVect = linAlgQuadPrecision_solveSPD(Kcov, Kstr)
 
     end function GP_volAvgToPointPredVect_
 
