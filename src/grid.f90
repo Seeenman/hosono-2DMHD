26f8db3aad222ae6cac803f709ad73dd85b43548
Sean Riedel <riedelsean@gmail.com>
2026-09-15 22:06:20 -0600

    Moving quadrature points and weights to GP module

--- src/grid.f90
+++ src/grid.f90
@@ -8,22 +8,14 @@ module grid
     ! computational domain) is in grid_block which is of type gridBlock_t
     ! (defined in gridBlock.f90) and includes indices, domain geometry,
     ! primitive and conservative variables at each cell, face centered
-    ! values, etc). What remains in this module as module variables
-    ! defined outside of grid_block are properties of the numerical method
-    ! such as the Gaussian process radius and related parameters
-    ! since these are not properties that vary depending on where
-    ! in the domain one is. 
+    ! values, etc).
 
     use definitions, only: max_string_length, ndim, xdir, ydir
     use readParamFile, only: readParamFile_int, readParamFile_real
     use gridBlock, only: gridBlock_t, gridBlock_alloc, gridBlock_dealloc
-    use GP, only: GP_radius
+    use GP, only: GP_radius, GP_nQuadrature
 
     implicit none
-
-    ! Quadrature
-    integer :: grid_nQuadrature ! number of face quadrature points
-    real, allocatable :: grid_quadPoints(:), grid_quadWeights(:)
 
     ! The grid for this run. See gridBlock.f90 for data in this struct
     type(gridBlock_t) :: grid_block
@@ -72,44 +64,10 @@ contains
             grid_block%dl(i_dim) = (grid_block%domainEnd(i_dim)-grid_block%domainBeg(i_dim))/grid_block%N(i_dim)
         end do
 
-        !!!! Set quadrature coordinates and quadrature weights
-        !!!! based on GP_radius
-        grid_nQuadrature = GP_radius+1
-        allocate(grid_quadPoints(grid_nQuadrature))
-        allocate(grid_quadWeights(grid_nQuadrature))
-        if (grid_nQuadrature == 2) then
-            ! GP spatial order of accuracy = 2*1+1 = 3
-            ! use 4th order, 2 point quadrature rule
-            grid_quadPoints(1) = 1.0/2.0/SQRT(3.0)
-            grid_quadPoints(2) = -grid_quadPoints(1)
-            grid_quadWeights(1) = 1.0/2.0
-            grid_quadWeights(2) = 1.0/2.0
-        else if (grid_nQuadrature == 3) then
-            ! GP spatial order of accuracy = 2*2+1 = 5
-            ! use 6th order, 3 point quadrature rule
-            grid_quadPoints(1) = 1.0/2.0*SQRT(3.0/5.0)
-            grid_quadPoints(2) = 0.0
-            grid_quadPoints(3) = -grid_quadPoints(1)
-            grid_quadWeights(1) = 5.0/18.0
-            grid_quadWeights(2) = 8.0/18.0
-            grid_quadWeights(3) = 5.0/18.0
-        else if (grid_nQuadrature == 4) then
-            ! GP spatial order of accuracy = 2*3+1 = 7
-            ! use 8th order, 4 point quadrature rule
-            grid_quadPoints(1) = 1.0/2.0*SQRT(3.0/7.0+2.0/7.0*SQRT(6.0/5.0))
-            grid_quadPoints(2) = 1.0/2.0*SQRT(3.0/7.0-2.0/7.0*SQRT(6.0/5.0))
-            grid_quadPoints(3) = -grid_quadPoints(2)
-            grid_quadPoints(4) = -grid_quadPoints(1)
-            grid_quadWeights(1) = (18.0-SQRT(30.0))/72.0
-            grid_quadWeights(2) = (18.0+SQRT(30.0))/72.0
-            grid_quadWeights(3) = (18.0+SQRT(30.0))/72.0
-            grid_quadWeights(4) = (18.0-SQRT(30.0))/72.0
-        end if
-
         !!!! allocate (and zero) the arrays belonging to the block, now
         !!!! that we know Nx, Ny, the guard cell count, and the
         !!!! number of quadrature points
-        call gridBlock_alloc(grid_block, grid_nQuadrature)
+        call gridBlock_alloc(grid_block, GP_nQuadrature)
 
         ! fill in grid points
         do i=grid_block%minIdx(xdir), grid_block%maxIdx(xdir)
@@ -136,8 +94,6 @@ contains
         ! ------------------------------------------------------------
         implicit none
         call gridBlock_dealloc(grid_block)
-        deallocate(grid_quadPoints)
-        deallocate(grid_quadWeights)
     end subroutine grid_finalize
 
 end module grid
